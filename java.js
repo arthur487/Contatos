6369059a570ea26c231a5a6edd9b56abcce75216
arthur <arthurdomiciano46@gmail.com>
2025-01-29 10:59:05 -0500

    finished

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function(){
+    $('#form-numero').on('submit', function(e) {
+    e.preventDefault();
+    const NomeDeTarefa = $('#Nome').val()
+    const NewItem = $('<li></li>');
+    $(`<ul>${NomeDeTarefa}</ul>`).appendTo(NewItem);
+    $(NewItem).appendTo('#Tbody')
+    $('#Nome').val('')
+    $(NewItem).click(function(){
+        $(NewItem).css("text-decoration","line-through")
+    })
+    $(NewItem).dblclick(function(){
+        $(NewItem).css("text-decoration","none");
+    })
+    })
+    $('ul').one("click",function(){
+        alert(`Double click to remove line through`)
+    })
+})
